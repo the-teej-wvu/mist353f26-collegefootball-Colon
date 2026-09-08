337c90634f84c6c668b0da9c249b6d390e7b3769
TJ Colon <tjc00030@mix.wvu.edu>
2026-09-08 17:41:26 -0400

    Created Login NandaSurendra

--- Data/CreateTables.sql
+++ Data/CreateTables.sql
@@ -1,5 +1,17 @@
+CREATE LOGIN NandaSurendra
+
+WITH PASSWORD = 'MI$T353Instructor';
+
+CREATE USER NandaSurendra
+
+FOR LOGIN NandaSurendra;
+
+ALTER ROLE db_owner ADD MEMBER NandaSurendra;
+
+/*
 create table test_table (
     id INT PRIMARY KEY,
     name VARCHAR(100),
     created_at TIMESTAMP
 );
+*/
